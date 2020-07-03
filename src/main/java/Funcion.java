e6d6965f5d42c9b34ca9fa8e861b1ad4f505a111
OsEstrada <00064318@uca.edu.sv>
2020-07-03 17:54:29 -0600

    formato

--- src/main/java/Funcion.java
+++ src/main/java/Funcion.java
@@ -1,18 +1,20 @@
 
 
 /**
- *
  * @author oscar
  */
-public class Funcion extends Simpson{
-   public double f(double x, double y){
-    return Math.sqrt(x)*Math.pow(y, 2);
-  }
-  public double c(double x){
-      return(-0.1);
-  }
-  public double d(double x){
-      return(0.1);
-  }
-    
+public class Funcion extends Simpson {
+    public double f(double x, double y) {
+        //return Math.sqrt(x) * Math.pow(y, 2);
+        return Math.pow(x,2) + Math.pow(y, 3);
+    }
+
+    public double c(double x) {
+        return (x);
+    }
+
+    public double d(double x) {
+        return (2*x);
+    }
+
 }
--- src/main/java/SimpsonDoble.java
+++ src/main/java/SimpsonDoble.java
@@ -6,7 +6,8 @@ public class SimpsonDoble {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        double resultado = new Funcion().integral(1.3, 1.5, 1, 2);
+        //double resultado = new Funcion().integral(1.3, 1.5, 1, 2);
+        double resultado = new Funcion().integral(2, 2.2, 1, 1);
         System.out.println("integral "+resultado);
     }
     
